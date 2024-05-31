f972867266d8bf1a372fa5cf9619abed95c554c4
Cody Keener <codymkeener@gmail.com>
2024-05-30 21:27:24 -0500

    Set up books to work with multiusers. Need to do the same for Authors next.

--- events/formEvents.js
+++ events/formEvents.js
@@ -3,7 +3,7 @@ import { createBook, updateBook, getBooks } from '../api/bookData';
 import { showAuthors } from '../pages/authors';
 import { showBooks } from '../pages/books';
 
-const formEvents = () => {
+const formEvents = (user) => {
   document.querySelector('#main-container').addEventListener('submit', (e) => {
     e.preventDefault();
     // CLICK EVENT FOR SUBMITTING FORM FOR ADDING A BOOK
@@ -15,11 +15,12 @@ const formEvents = () => {
         price: document.querySelector('#price').value,
         author_id: document.querySelector('#author_id').value,
         sale: document.querySelector('#sale').checked,
+        uid: user.uid
       };
       createBook(payload).then(({ name }) => {
         const patchPayload = { firebaseKey: name };
         updateBook(patchPayload).then(() => {
-          getBooks().then(showBooks);
+          getBooks(user).then(showBooks);
         });
       });
     }
